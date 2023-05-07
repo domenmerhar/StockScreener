085710b657afb5a08844446d6a0e39c11406d9e0
domenmerhar <merhardomen@gmail.com>
2023-05-07 19:23:18 +0200

    Acquired Stock Info

--- main.py
+++ main.py
@@ -6,9 +6,23 @@ apiKey = open("apiKey.txt", "r").read()
 ticker = "META"
 years = 1
 
+# Get stock info
+marketCap = requests.get(
+    f"https://financialmodelingprep.com/api/v3/market-capitalization/{ticker}?apikey={apiKey}")
 incomeStatement = requests.get(
     f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&apikey={apiKey}")
+balanceSheet = requests.get(
+    f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&apikey={apiKey}")
+cashFlowStatement = requests.get(
+    f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={years}&apikey={apiKey}")
 
+# Convert to .json file
+marketCap = marketCap.json()
 incomeStatement = incomeStatement.json()
+balanceSheet = balanceSheet.json()
+cashFlowStatement = cashFlowStatement.json()
 
-print(incomeStatement)
+print(marketCap[0]["marketCap"])
+print(incomeStatement[0])
+print(balanceSheet[0])
+print(cashFlowStatement[0])
