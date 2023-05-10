0196a405ef4164f59fb9b4473565dd210c80b1dd
domenmerhar <merhardomen@gmail.com>
2023-05-10 19:50:19 +0200

    Added Function

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ apiKey = open("apiKey.txt", "r").read()
 ticker = "META"
 years = 1
 
-# Get stock info
-# marketCap = requests.get(f"https://financialmodelingprep.com/api/v3/market-capitalization/{ticker}?apikey={apiKey}")
-# incomeStatement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&apikey={apiKey}")
-# balanceSheet = requests.get(f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&apikey={apiKey}")
-# cashFlowStatement = requests.get(f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={years}&apikey={apiKey}")
-financialRatios = requests.get(
-    f"https://financialmodelingprep.com/api/v3/ratios-ttm/{ticker}?apikey={apiKey}")
 
-# Convert to .json file
-# marketCap = marketCap.json()
-# incomeStatement = incomeStatement.json()
-# balanceSheet = balanceSheet.json()
-# cashFlowStatement = cashFlowStatement.json()
-financialRatios = financialRatios.json()
+def GetInfo(years: int, ticker: str):  # Hints help document the parameters
+    # Get stock info
+    # marketCap = requests.get(f"https://financialmodelingprep.com/api/v3/market-capitalization/{ticker}?apikey={apiKey}")
+    # incomeStatement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&apikey={apiKey}")
+    # balanceSheet = requests.get(f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&apikey={apiKey}")
+    # cashFlowStatement = requests.get(f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={years}&apikey={apiKey}")
+    financialRatios = requests.get(
+        f"https://financialmodelingprep.com/api/v3/ratios-ttm/{ticker}?apikey={apiKey}")
 
-with open('results.txt', 'w') as f:
-    for key in financialRatios[0]:
-        f.write(f"{key}: {financialRatios[0][key]}\n")
+    # Convert to .json file
+    # marketCap = marketCap.json()
+    # incomeStatement = incomeStatement.json()
+    # balanceSheet = balanceSheet.json()
+    # cashFlowStatement = cashFlowStatement.json()
+    financialRatios = financialRatios.json()
 
-# print(financialRatios)
+    with open(f"{ticker}.txt", "w") as f:
+        # Cycle through every item in the 2nd dimension
+        for ratio in financialRatios[0]:
+            f.write(f"{ratio}: {financialRatios[0][ratio]}\n")
+
+
+GetInfo(1, "GTN")
