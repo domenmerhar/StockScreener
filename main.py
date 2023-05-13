84d4498dec0f023c112e3fea621e16b09e261ac4
domenmerhar <merhardomen@gmail.com>
2023-05-13 12:38:14 +0200

    Added Growth Ratios

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import matplotlib
 
 apiKey = open("apiKey.txt", "r").read()
 
-ticker = "META"
-years = 1
+ticker = "MSFT"
+years = 5
 
 
 def WipeTxt(ticker: str):
@@ -17,7 +17,7 @@ def AppendMarketCap(marketCap: dict, ticker: str):
         file.write(f"Market Cap: {marketCap[0]['marketCap']}\n\n")
 
 
-def AppendRatios(financialStatement: dict, years: int, cutList: bool, ticker: str, typeOfStatement: str):
+def AppendRatios(financialStatement: dict, cutList: bool, ticker: str, typeOfStatement: str, years: int = 1):
     with open(f"Output\{ticker}.txt", "a") as file:
         file.write(f"****{typeOfStatement.capitalize()}****")
 
@@ -40,32 +40,43 @@ def AppendRatios(financialStatement: dict, years: int, cutList: bool, ticker: st
             file.write("\n")
 
 
-def GetInfo(years: int, ticker: str):  # Hints help document the parameters
+def FinancialRatioGrowth(incomeGrowthStatement: dict, financialRatios: str, years: int):
+    periodGrowth = 1
+    for year in range(years - 1):
+        FYgrowth = int(incomeGrowthStatement[year][financialRatios])
+        if (FYgrowth < 0):
+            periodGrowth += FYgrowth * periodGrowth
+        else:
+            periodGrowth *= FYgrowth + 1
+    return periodGrowth * 100
+
+
+def GetInfo(years: int, ticker: str, apiKey: str):  # Hints help document the parameters
     # Get stock info
     marketCap = requests.get(
-        f"https://financialmodelingprep.com/api/v3/market-capitalization/{ticker}?apikey={apiKey}")
+        f"https://financialmodelingprep.com/api/v3/market-capitalization/{ticker}?apikey={apiKey}").json()
     incomeStatement = requests.get(
-        f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&apikey={apiKey}")
+        f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&apikey={apiKey}").json()
     balanceSheet = requests.get(
-        f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&apikey={apiKey}")
+        f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&apikey={apiKey}").json()
     cashFlowStatement = requests.get(
-        f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={years}&apikey={apiKey}")
+        f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={years}&apikey={apiKey}").json()
     financialRatios = requests.get(
-        f"https://financialmodelingprep.com/api/v3/ratios-ttm/{ticker}?apikey={apiKey}")
-
-    # Convert to .json file
-    marketCap = marketCap.json()
-    incomeStatement = incomeStatement.json()
-    balanceSheet = balanceSheet.json()
-    cashFlowStatement = cashFlowStatement.json()
-    financialRatios = financialRatios.json()
+        f"https://financialmodelingprep.com/api/v3/ratios-ttm/{ticker}?apikey={apiKey}").json()
 
     WipeTxt(ticker)
-    AppendMarketCap(marketCap, ticker)
-    AppendRatios(incomeStatement, years, True, ticker, "Income Statement")
-    AppendRatios(balanceSheet, years, True, ticker, "Balance Sheet")
-    AppendRatios(cashFlowStatement, years, True, ticker, "Cash Flow Statement")
-    AppendRatios(financialRatios, years, True, ticker, "Financial Ratios")
+    # AppendMarketCap(marketCap, ticker)
+    # AppendRatios(incomeStatement, True, ticker, "Income Statement", years)
+    # AppendRatios(balanceSheet, True, ticker, "Balance Sheet", years)
+    # AppendRatios(cashFlowStatement, True, ticker, "Cash Flow Statement", years)
+    # AppendRatios(financialRatios, True, ticker, "Financial Ratios", years)
+
+    incomeGrowth = requests.get(
+        f"https://financialmodelingprep.com/api/v3/income-statement-growth/{ticker}?limit={years}&apikey={apiKey}").json()
+
+    with open(f"Output\{ticker}.txt", "a") as file:
+        file.write(
+            f"{FinancialRatioGrowth(incomeGrowth, 'growthEPS', years)}%\n")
 
 
-GetInfo(1, "CROX")
+GetInfo(years, ticker, apiKey)
